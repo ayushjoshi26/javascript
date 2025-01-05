414ed9d8aa2b23f12c394ff2d79dc3a7c8dc70ff
Ayush joshi <joshiayush250@gmail.com>
2025-01-05 15:32:57 +0000

    datatypes

---
+++ javascript/datatype.js
@@ -0,0 +1,24 @@
+//Datatype is ensure your computer to which type of value stored by the user in his variable
+
+/* 
+    There are diffrent type of datatype: 
+    -> string
+    -> Number
+    -> boolean: true or false
+    -> bigint: Its is used to store the large interger value. 
+    -> Null: Its is a representation of empty value.
+    -> undefined: when you not set the value.
+    -> symbol: unique
+*/
+
+let number = 14;
+let string = "Ayush joshi"
+let isfalse = true
+
+console.table([number, string, isfalse])
+
+
+/*
+    -> null: when you check the type of the null so why the output is object, because it is a 
+             historical bug in js.
+*/
