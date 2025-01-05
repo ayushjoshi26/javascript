a2483b453b6d440546a097ac513ff4a09387b5cd
Ayush joshi <joshiayush250@gmail.com>
2025-01-05 10:59:56 +0000

    container config

---
+++ javascript/variable.js
@@ -0,0 +1,10 @@
+const account_id = 123456;
+let account_name = "Ayush joshi";
+var city_name = "new delhi";
+
+console.table([account_name, account_id, city_name]); // this is used to print in a tabular form.
+
+/* 
+   let:  
+
+*/
