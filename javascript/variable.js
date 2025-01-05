1ebc1ccb3a9f0f9cbd83b126bcf0e9ea113b6623
Ayush joshi <joshiayush250@gmail.com>
2025-01-05 11:05:01 +0000

    container config

--- javascript/variable.js
+++ javascript/variable.js
@@ -1,5 +1,5 @@
-const account_id = 123456;
-let account_name = "Ayush joshi";
+const account_id = 123456; //when you use this so you cant change the value.
+let account_name = "Ayush joshi"; // this is highly recommend to use this variable.
 var city_name = "new delhi";
 
 console.table([account_name, account_id, city_name]); // this is used to print in a tabular form.
