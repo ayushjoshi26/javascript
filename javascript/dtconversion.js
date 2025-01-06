8c7c74ed671627edc1fcb5d30d5ccdcc70cf7c1f
Ayush joshi <joshiayush250@gmail.com>
2025-01-06 06:44:29 +0000

    datatypeconversion

---
+++ javascript/dtconversion.js
@@ -0,0 +1,20 @@
+let score = "45abc";
+// console.log(score);
+// console.log(typeof (score));
+
+let stringtoNumber = Number(score);
+// console.log(stringtoNumber)
+// console.log(typeof (stringtoNumber))
+
+//"45abc": NaN
+//"45": Number
+
+let userLoggedin = "Ayush";
+console.log(Boolean(userLoggedin))
+console.log(userLoggedin)
+
+//1: true, 0: false
+//"": false
+//"Ayush": true
+
+
