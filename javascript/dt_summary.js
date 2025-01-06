2daea1fc2e4d6d5124f715d23e2073c8bbc074ac
Ayush joshi <joshiayush250@gmail.com>
2025-01-06 09:30:39 +0000

    DT_summary

---
+++ javascript/dt_summary.js
@@ -0,0 +1,29 @@
+// primitive Datatype 
+// there are 7 type of datatype: string ,boolean ,number ,undefined ,null ,symbol ,Bigint
+
+const string = "Ayush"
+const boolean = true
+const number = 12
+let undefined;
+const num = null;
+const sym = Symbol
+const Bigint = 123344654654667457n
+
+
+//Javascript is dynamically language
+
+// Non-primitive Datatype: object, function , array 
+
+const superHeroes = ["spiderman", "superman", "thor"];
+
+const intro = {
+    Name: "Ayush joshi",
+    age: 19, 
+    isPass: true
+}
+
+const sumfunction = function (){
+    comsole.log("hello world");
+}
+
+console.log(sumfunction)
