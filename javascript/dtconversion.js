26b8bf9eae2dd7068093dda508418e184da0036e
Ayush joshi <joshiayush250@gmail.com>
2025-01-06 07:21:00 +0000

    datatype conversion

--- javascript/dtconversion.js
+++ javascript/dtconversion.js
@@ -10,11 +10,37 @@ let stringtoNumber = Number(score);
 //"45": Number
 
 let userLoggedin = "Ayush";
-console.log(Boolean(userLoggedin))
-console.log(userLoggedin)
+// console.log(Boolean(userLoggedin))
+// console.log(userLoggedin)
 
 //1: true, 0: false
 //"": false
 //"Ayush": true
+
+
+//******************* Operations ***********************//
+
+let num = 5;
+let negvalue = -num
+console.log(negvalue);
+
+/* 
+   -> basic math operation:
+    + plus
+    - subsctraction
+    * multiplication
+    / division
+    % remainder
+    **to the power
+*/
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log(1 + 2 + "5") because js is give the priority to the first two number and convert then into string, and the output is 35.
+// console.log(1 + "2" + 5)
+
+//++x plus 1 before the value
+//x++ plus 1 after the value
+
 
 
