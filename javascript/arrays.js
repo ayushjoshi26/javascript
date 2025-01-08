9b7c445d3b877883a866dec904850c0491aa20ff
Ayush joshi <joshiayush250@gmail.com>
2025-01-08 07:42:41 +0000

    arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,20 @@
+//arrays: Its a non primitive datatype.
+
+const arr1 = [0,1,2,3,4,5] // this a array declaration
+const myHeros = ["spiderman","ironman","Thor"]
+let myArr2 = Array(1,2,3,4,5)
+// console.log(myHeros[1]); // you can print the arrays value using 'myHeros[]'.
+
+//arrays methods:
+
+// arr1.push(7) // It is used to add the value form the ending.
+// arr1.pop()// It is used to remove the value form the ending.
+// arr1.unshift(12) // It is used to add the value from the starting.
+// arr1.shift() // It is used to remove the value from the starting.`
+
+// console.log(arr1.includes(9)) // It is used to the find the value is exist in the array, its gives the boolean value.
+// console.log(arr1.indexOf(4)) // It is show the index number of the value that you insert in it.
+
+let newArr = arr1.join() // its show the array value in a string. 
+console.log(arr1)
+console.log(newArr)
