945b2b54f71cd43c058fcc948d04c77657565f89
Ayush joshi <joshiayush250@gmail.com>
2025-01-09 17:36:56 +0000

    function

---
+++ javascript/function.js
@@ -0,0 +1,30 @@
+//function 
+
+function sayMyname() {
+    console.log("a")
+    console.log("y")
+    console.log("u")
+    console.log("s")
+    console.log("h")
+}
+
+// sayMyname()
+
+function addTwoNumbers(number1, number2){
+    // console.log(number1 + number2)
+    // let result = number1+ number2
+    return number1+ number2
+}
+
+// console.log("result = ", addTwoNumbers(2,3))
+
+// when someone not give the parameter in the function calling so the output is undefined.
+function userloginInfo (username){
+    return `${username} just logged in.`
+}
+
+console.log(userloginInfo("Ayush"))
+
+
+
+
