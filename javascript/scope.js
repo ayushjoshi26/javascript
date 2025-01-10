91d01533c3d0645968eecb3543927078a6624d77
Ayush joshi <joshiayush250@gmail.com>
2025-01-10 07:24:56 +0000

    scope

--- javascript/scope.js
+++ javascript/scope.js
@@ -10,3 +10,28 @@
 
 //block scope variables are only accessible within the block they are defined in.
 //Global scope variables are accessible anywhere in the program.
+
+
+function one(){
+    const username = "Ayush joshi"
+    function two (){
+        const Website = "youtube"
+        console.log(username)
+    }
+    // console.log(Website)
+    // two()
+}
+// one()
+
+if (true){
+    const Name = "ayush joshi"
+    if(Name === "ayush joshi"){
+        const website = " youtube"
+        console.log(Name + website)
+    }
+    // console.log(website)
+    console.log(Name)
+}
+
+
+// if you wanna print the value so you need to remember the scopes. 
