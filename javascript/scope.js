e11ca8a4a97502be5bc042c225c856899bcc08cb
Ayush joshi <joshiayush250@gmail.com>
2025-01-10 06:23:22 +0000

    scope

---
+++ javascript/scope.js
@@ -0,0 +1,12 @@
+// scopes
+// there are two type of scope: .Global .Block
+
+
+/*
+    if(){
+        //this part is the block scope.
+    }
+*/
+
+//block scope variables are only accessible within the block they are defined in.
+//Global scope variables are accessible anywhere in the program.
